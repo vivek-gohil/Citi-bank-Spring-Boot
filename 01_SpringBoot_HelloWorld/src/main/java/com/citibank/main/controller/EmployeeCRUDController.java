b2d39556d8f3bfdb6bcc5a9436988fd810fa7f47
Vivek Gohil <59043694+vivek-gohil@users.noreply.github.com>
2024-10-09 15:26:07 +0530

    Day 3 Updated

--- 01_SpringBoot_HelloWorld/src/main/java/com/citibank/main/controller/EmployeeCRUDController.java
+++ 01_SpringBoot_HelloWorld/src/main/java/com/citibank/main/controller/EmployeeCRUDController.java
@@ -3,7 +3,11 @@ package com.citibank.main.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,10 +34,40 @@ public class EmployeeCRUDController {
 		employeeList.add(employee2);
 	}
 
-	//http://localhost:8080/crudapi/employee
+	// http://localhost:8080/crudapi/employee - HTTP Method = Get
 	@GetMapping("employee")
 	public List<Employee> getAllEmployees() {
 		return employeeList;
 	}
 
+	// http://localhost:8080/crudapi/employee/{employeeId} - HTTP Method = Get
+	@GetMapping("employee/{employeeId}")
+	public Employee getEmployeeByEmployeeId(@PathVariable int employeeId) {
+		for (Employee e : employeeList) {
+			if (e.getEmployeeId() == employeeId) {
+				return e;
+			}
+		}
+		return null;
+	}
+
+	// http://localhost:8080/crudapi/employee - HTTP Method = Post
+	@PostMapping("employee")
+	public boolean addNewEmployee(@RequestBody Employee employee) {
+		boolean result;
+		result = employeeList.add(employee);
+		return result;
+	}
+
+	// http://localhost:8080/crudapi/employee/{employeeId} - HTTP Method = Delete
+	@DeleteMapping("employee/{employeeId}")
+	public boolean deleteEmployeeByEmployeeId(@PathVariable int employeeId) {
+		for(Employee e : employeeList) {
+			if(e.getEmployeeId() == employeeId) {
+				employeeList.remove(e);
+				return true;
+			}
+		}
+		return false;
+	}
 }
